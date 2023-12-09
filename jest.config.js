f8a4bbd9a44a7889cbec2068364e45692daebb8d
temelehtone <91484193+temelehtone@users.noreply.github.com>
2023-12-09 18:03:31 +0200

    remove jest reporter

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,3 @@
 export default {
-  collectCoverageFrom: ["src/testable/**/*.js"],
-  "reporters": [
-    ["jest-html-reporters", { "filename": "test-report.html" }]
-  ]
+  collectCoverageFrom: ["src/testable/**/*.js"]
 };
